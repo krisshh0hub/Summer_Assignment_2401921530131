0fb162310c29a9c29603b190dd09fa4a3626f54f
krisshh0hub <24113cn220@glbitm.ac.in>
2026-06-03 18:48:14 +0530

    Create q2.cpp

---
+++ week1/day3/q2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> v(n);
+
+        int i = 0;
+        int j = n - 1;
+        int k = n - 1;
+
+        while (i <= j) {
+
+            if (abs(nums[i]) > abs(nums[j])) {
+                v[k] = nums[i] * nums[i];
+                i++;
+            }
+            else {
+                v[k] = nums[j] * nums[j];
+                j--;
+            }
+
+            k--;
+        }
+
+        return v;
+    }
+};
