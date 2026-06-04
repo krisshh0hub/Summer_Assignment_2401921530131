c8694a337abfc83c4c141f85e360db8e3b242125
krisshh0hub <24113cn220@glbitm.ac.in>
2026-06-04 22:24:04 +0530

    Create q1.cpp

---
+++ week1/day4/q1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int sum=0;int n=mat.size();
+        for(int i=0;i<mat.size();i++){
+            sum+=mat[i][i];
+        }
+        for(int i=0;i<n;i++){
+            sum+=mat[i][n-1-i];
+        }
+        if(n%2!=0){
+            sum=sum-mat[n/2][n/2];
+        }
+        return sum;
+    }
+};
