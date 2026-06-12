cf5007a43943e7e601e5a44c3e9748448d15c3f8
krisshh0hub <24113cn220@glbitm.ac.in>
2026-06-12 21:23:04 +0530

    Update longestSubstr.cpp

--- week2/day2/longestSubstr.cpp
+++ week2/day2/longestSubstr.cpp
@@ -19,3 +19,24 @@ public:
         return maxi;
     }
 };
+
+
+
+//left pointer to shrink window , right(i here) one to inc window size
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> st;
+        int left = 0, ans = 0;
+        for(int i = 0; i < s.size(); i++) {
+            while(st.count(s[i])) {
+                st.erase(s[left]);// jab tak repeating character present hai ,becz we wanna substring
+                left++;
+            }
+            st.insert(s[i]);
+            ans = max(ans, i - left + 1);
+        }
+        return ans;
+    }
+};
