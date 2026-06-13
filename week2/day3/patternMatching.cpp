12302c963efb65fd7853ad733fe15c03f4a4df85
krisshh0hub <24113cn220@glbitm.ac.in>
2026-06-13 14:38:47 +0530

    Update patternMatching.cpp

--- week2/day3/patternMatching.cpp
+++ week2/day3/patternMatching.cpp
@@ -1,4 +1,4 @@
-//using stlclass Solution {
+//using stl class Solution {
 public:
     int strStr(string haystack, string needle) {
         int n=needle.size();
@@ -10,3 +10,30 @@ public:
         return -1;
     }
 };
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.size() > haystack.size())
+            return -1;
+        for(int i=0;i<=haystack.size()-needle.size();i++){//index over flow se bachne ke liye..if i=8 and j=2 ..i+j=10 ,out of bound in haysatack
+            //check needle ka j element haystack ke i se match kr rha ya nhi.
+            int j=0;bool flag=true;
+            while(j<needle.size()){
+                if(haystack[i+j]==needle[j]){
+                    j++;
+                }else{
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag==true)
+            return i;
+        }
+        return -1;
+    }
+};
+
+// Knuth-Morris-Pratt (KMP) algorithm.
+
+ 
